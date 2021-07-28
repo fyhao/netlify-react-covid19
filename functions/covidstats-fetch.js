9a081546c93967ca11989cf8bf574306ee578651
fyhao <fyhao1@gmail.com>
2021-07-28 22:35:58 +0800

    covidstats-fetch use back helper

--- functions/covidstats-fetch.js
+++ functions/covidstats-fetch.js
@@ -1,30 +1,5 @@
 const fetch = require("node-fetch");
-var admin = require("firebase-admin");
-var db = null;
-var serviceAccount = null;
-console.log('init firebase');
-if(process.env.NODE_ENV !== 'production') {
-	require('dotenv').config({ path: '.env.local' });
-	serviceAccount = JSON.parse(require('fs').readFileSync('serviceAccountKey.json'));
-		
-	admin.initializeApp({
-	  credential: admin.credential.cert(serviceAccount)
-	});
-	db = admin.firestore()
-}
-else {
-	const response = fetch(process.env.FIREBASE_NETLIFY_COVIDSTATS_URL)
-		.then(response => response.json())
-		.then(data => {
-			serviceAccount = data;
-			admin.initializeApp({
-			  credential: admin.credential.cert(serviceAccount)
-			});
-			db = admin.firestore()
-		});
-    
-}
-
+const db = require('./utils/firebase');
 const API_URL = process.env.COVIDSTAT_APIURL;
 
 
