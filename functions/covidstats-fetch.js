8e3da15f01053dcab39f207d9fb0b1291ea1a61a
fyhao <fyhao1@gmail.com>
2021-07-28 19:39:10 +0800

    #5 fix

--- functions/covidstats-fetch.js
+++ functions/covidstats-fetch.js
@@ -1,24 +1,33 @@
 const fetch = require("node-fetch");
 
+var admin = require("firebase-admin");
+var db = null;
 var serviceAccount = null;
+console.log('init firebase');
 if(process.env.NODE_ENV !== 'production') {
 	require('dotenv').config({ path: '.env.local' });
 	serviceAccount = JSON.parse(require('fs').readFileSync('serviceAccountKey.json'));
+		
+	admin.initializeApp({
+	  credential: admin.credential.cert(serviceAccount)
+	});
+	db = admin.firestore()
 }
 else {
-	const response = await fetch(process.env.FIREBASE_NETLIFY_COVIDSTATS_URL);
-    var data = await response.json();
-	serviceAccount = data;
+	const response = fetch(process.env.FIREBASE_NETLIFY_COVIDSTATS_URL)
+		.then(response => response.json())
+		.then(data => {
+			serviceAccount = data;
+			admin.initializeApp({
+			  credential: admin.credential.cert(serviceAccount)
+			});
+			db = admin.firestore()
+		});
+    
 }
 const API_URL = process.env.COVIDSTAT_APIURL;
 
-var admin = require("firebase-admin");
 
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-const db = admin.firestore()
 exports.handler = async function (event, context) {
 	
   var mockeddata = [{"totalcases":"191712967","death":"4112792","recovered":"174567234","datetime":"2021-07-20 11:50:02"},{"totalcases":"191712967","death":"4112792","recovered":"174567234","datetime":"2021-07-20 11:40:02"}];
