627be5574b7d81717a503e83f9201d74d5ebfc71
fyhao <fyhao1@gmail.com>
2021-07-28 19:26:59 +0800

    #5 use different way to host serviceaccount

--- functions/covidstats-fetch.js
+++ functions/covidstats-fetch.js
@@ -6,18 +6,9 @@ if(process.env.NODE_ENV !== 'production') {
 	serviceAccount = JSON.parse(require('fs').readFileSync('serviceAccountKey.json'));
 }
 else {
-	serviceAccount = {
-	  "type": "service_account",
-	  "project_id": process.env.FIREBASE_PROJECT_ID,
-	  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-	  "private_key": process.env.FIREBASE_PRIVATE_KEY,
-	  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-	  "client_id": process.env.FIREBASE_CLIENT_ID,
-	  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-	  "token_uri": "https://oauth2.googleapis.com/token",
-	  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-	  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
-	};
+	const response = await fetch(process.env.FIREBASE_NETLIFY_COVIDSTATS_URL);
+    var data = await response.json();
+	serviceAccount = data;
 }
 const API_URL = process.env.COVIDSTAT_APIURL;
 
